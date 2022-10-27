39b5dd020e7f422105e369cb39d85f7b647b76ba
Angel Terziev <angel.terziev20@gmail.com>
2022-10-27 23:14:06 +0300

    14 wonders, can have imporvements

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const gallery = document.getElementById("gallery");
 
   window.onmousemove = (e) => {
-    const mouseX = e.clientX,
-      mouseY = e.clientY;
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
 
-    const xDecimal = mouseX / window.innerWidth,
-      yDecimal = mouseY / window.innerHeight;
+    const xDecimal = mouseX / window.innerWidth;
+    const yDecimal = mouseY / window.innerHeight;
 
-    const maxX = gallery.offsetWidth - window.innerWidth,
-      maxY = gallery.offsetHeight - window.innerHeight;
+    const maxX = gallery.offsetWidth - window.innerWidth;
+    const maxY = gallery.offsetHeight - window.innerHeight;
 
-    const panX = maxX * xDecimal * -1,
-      panY = maxY * yDecimal * -1;
+    const panX = maxX * xDecimal * -1.5;
+    const panY = maxY * yDecimal * -1.1;
 
     gallery.animate(
       {
