84afffea6c79ab529bf2f35a9bebc146fb966514
aterziev20 <angel.terziev20@gmail.com>
2022-11-08 23:54:36 +0200

    Info + not working arrows

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // preloader
+
   let loader = document.querySelector("#preloader");
   window.addEventListener("load", () => {
     loader.style.display = "none";
   });
 
+  // onmousemove effect
+
   const gallery = document.querySelector(".gallery");
 
-  window.onmousemove = (e) => {
+  window.addEventListener("mousemove", (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
 
@@ -29,5 +33,54 @@ document.addEventListener("DOMContentLoaded", function () {
         easing: "ease",
       }
     );
-  };
+  });
+
+  // On click of image make another div visible
+
+  let preveiwContainer = document.querySelector(".info");
+  let previewBox = preveiwContainer.querySelectorAll(".image-info");
+
+  document.querySelectorAll(".gallery .tile").forEach((product) => {
+    product.onclick = () => {
+      preveiwContainer.style.display = "flex";
+      let name = product.getAttribute("data-name");
+      previewBox.forEach((preview) => {
+        let target = preview.getAttribute("data-target");
+        if (name == target) {
+          preview.classList.add("active");
+        }
+      });
+    };
+  });
+
+  // close info
+
+  previewBox.forEach((close) => {
+    close.querySelector(".fa-xmark").onclick = () => {
+      close.classList.remove("active");
+      preveiwContainer.style.display = "none";
+    };
+    window.onkeyup = (event) => {
+      if (event.keyCode == 27) {
+        close.classList.remove("active");
+        preveiwContainer.style.display = "none";
+      }
+    };
+  });
+
+  // Arrows to see different image
+
+  const left = document.getElementById("leftArrow");
+  const right = document.getElementById("rightArrow");
+
+  left.addEventListener("click", () => {
+    if (target > 1) {
+      target + 1;
+    }
+  });
+  right.addEventListener("click", () => {
+    if (target < 1) {
+      images.length++;
+    }
+  });
 });
